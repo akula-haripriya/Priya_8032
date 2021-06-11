9c4172713a65862ab23b9f0ff84d1562b9f95669
akula-haripriya <hakula@gitam.in>
2021-06-11 13:12:42 +0530

    python

---
+++ problem.py/1st problem.py
@@ -0,0 +1,13 @@
+n=int(input("Enter any Number"))
+a=str(n)
+even=0
+odd=0 
+for i in a:
+      b=int(i)
+      if b%2==0:
+            even+=1
+      else:
+            odd+=1
+print("No of even digits", even)
+print("No of odd digits", odd)
+
